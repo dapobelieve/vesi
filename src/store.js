db606c2b96ba312c88b7a828a663b9410897b645
Dapo Michaels <dapomichaels@gmail.com>
2021-02-16 01:43:25 +0100

    remove ddata

--- src/store.js
+++ src/store.js
@@ -1,13 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
-// import defaultData from './data'
 import { uuid, saveState } from './utils'
 
 Vue.use(Vuex)
 
-
-
-const board = JSON.parse(localStorage.getItem('board')) || defaultData
 export default new Vuex.Store({
 	plugins: [saveState],
 	state: {
